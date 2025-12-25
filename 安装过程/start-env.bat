0a2b0eb9395c03661f707541148f46304bab4294
Kusion <1599015491@qq.com>
2025-12-25 23:23:02 +0800

    install require

---
+++ 安装过程/start-env.bat
@@ -0,0 +1,4 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+cd /d G:\vsr-source-compile\ProPainter-Webui\propainterEnv
+cmd /k Scripts\activate
